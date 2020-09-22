c4c2d183fdcb3b38e4e4cf636e27b535ab2559bb
Glacier-T <1713828723@qq.com>
2020-09-22 23:01:00 +0800

    到入文章清理，ik分词器division调用优化

--- src/main/java/Util/IK_words_divider.java
+++ src/main/java/Util/IK_words_divider.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-public class IKUtil {
+public class IK_words_divider {
 
     public static void main(String[] args) throws IOException {
 
     }
-//分词
+    //分词
     public List<String> division(String str) throws IOException {
         StringReader stringReader=new StringReader(str);//将句子读成流
         IKSegmenter ikSegmenter=new IKSegmenter(stringReader,true);//将流形式的文字传入ik分词器
@@ -26,13 +26,16 @@ public class IKUtil {
         }
         return list;
     }
-//引入句子
+    
+    //引入句子
     public List<String> phrase(String str1,String str2) throws IOException {
-        List<String> division1 = division(str1);//将str1文字串分词后用list列表装起来
-        List<String> division2 = division(str2);
+        StringBuilder stringBuilder=new StringBuilder();//将str1于srt2合并一个字符流，调用一次division
+        stringBuilder.append(str1);
+        stringBuilder.append(str2);
+        List<String> division = division(stringBuilder.toString());
         HashSet<String> hashSet=new HashSet<String>();//new一个hashset对象
-        division1.forEach(str->{hashSet.add(str);});//对division1的每一个分词都引入hashset对象
-        division2.forEach(str->{hashSet.add(str);});
+        division.forEach(str->{hashSet.add(str);});
         return Arrays.asList(hashSet.toArray(new String[0]));//以数组形式返回合并去重的词
     }
+    
 }
